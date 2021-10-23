83920536d60331879bba4800e827f8976eb2e207
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-10-23 22:53:18 +0200

    add command String add test command functionality

--- src/main/java/me/frauenfelderflorian/wuprojects/commands/CAllItems.java
+++ src/main/java/me/frauenfelderflorian/wuprojects/commands/CAllItems.java
@@ -1,5 +1,6 @@
 package me.frauenfelderflorian.wuprojects.commands;
 
+import me.frauenfelderflorian.wuprojects.projects.AllItems;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -7,9 +8,12 @@ import org.bukkit.command.TabExecutor;
 import java.util.List;
 
 public class CAllItems implements TabExecutor {
+    public static String command = "allitems";
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
-        return false;
+        new AllItems();
+        return true;
     }
 
     @Override
