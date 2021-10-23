65747a527e913ae9e135b525246a5d124cc1d587
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-10-23 22:53:40 +0200

    use command String

--- src/main/java/me/frauenfelderflorian/wuprojects/WUProjects.java
+++ src/main/java/me/frauenfelderflorian/wuprojects/WUProjects.java
@@ -6,11 +6,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Objects;
 
 public final class WUProjects extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        Objects.requireNonNull(getCommand("allitems")).setExecutor(new CAllItems());
-        Objects.requireNonNull(getCommand("allitems")).setTabCompleter(new CAllItems());
+        Objects.requireNonNull(getCommand(CAllItems.command)).setExecutor(new CAllItems());
+        Objects.requireNonNull(getCommand(CAllItems.command)).setTabCompleter(new CAllItems());
     }
 
     @Override
