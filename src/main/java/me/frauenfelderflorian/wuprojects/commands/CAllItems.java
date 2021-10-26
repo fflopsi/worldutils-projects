e20ea4e2dbdd04aa124eb6b0e99aa1147c17baec
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-10-26 17:22:48 +0200

    add handling to arguments new and skip

--- src/main/java/me/frauenfelderflorian/wuprojects/commands/CAllItems.java
+++ src/main/java/me/frauenfelderflorian/wuprojects/commands/CAllItems.java
@@ -1,7 +1,10 @@
 package me.frauenfelderflorian.wuprojects.commands;
 
+import me.frauenfelderflorian.worldutils.config.Prefs;
 import me.frauenfelderflorian.wuprojects.WUProjects;
 import me.frauenfelderflorian.wuprojects.projects.AllItems;
+import org.apache.commons.lang.WordUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -13,8 +16,28 @@ public record CAllItems(WUProjects plugin) implements TabExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
-        plugin.allItems = new AllItems(plugin);
-        return true;
+        if (args.length == 1)
+            switch (args[0]) {
+                case "new" -> {
+                    if (plugin.utils.prefs.getInt(Prefs.Option.WUP_ALLITMES_INDEX) == 0) {
+                        plugin.allItems = new AllItems(plugin);
+                        Bukkit.broadcastMessage("§bNew AllItems project started!");
+                    } else {
+                        sender.sendMessage("§ePlease reset the running AllItems project before starting a new one.");
+                        sender.sendMessage("§cType \"/allitems reset\" to reset the ongoing AllItems project.");
+                    }
+                    return true;
+                }
+                case "skip" -> {
+                    sender.sendMessage("§eSkipping " + WordUtils.capitalizeFully(
+                            plugin.utils.prefs.getList(Prefs.Option.WUP_ALLITMES_ITEMS)
+                                    .get(plugin.utils.prefs.getInt(Prefs.Option.WUP_ALLITMES_INDEX) - 1)
+                                    .toString().replace('_', ' ')));
+                    plugin.allItems.update(true);
+                    return true;
+                }
+            }
+        return false;
     }
 
     @Override
