4c22be862ea7365fde93d8fa201098c0ccb2bf39
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-10-27 18:18:20 +0200

    add handling of argument reset

--- src/main/java/me/frauenfelderflorian/wuprojects/commands/CAllItems.java
+++ src/main/java/me/frauenfelderflorian/wuprojects/commands/CAllItems.java
@@ -18,7 +18,7 @@ public record CAllItems(WUProjects plugin) implements TabExecutor {
         if (args.length == 1)
             switch (args[0]) {
                 case "new" -> {
-                    if (plugin.utils.prefs.getInt(Prefs.Option.WUP_ALLITMES_INDEX) == 0) {
+                    if (plugin.utils.prefs.getList(Prefs.Option.WUP_ALLITMES_ITEMS) == null) {
                         plugin.allItems = new AllItems(plugin);
                         Bukkit.broadcastMessage("§bNew AllItems project started!");
                     } else {
@@ -30,6 +30,16 @@ public record CAllItems(WUProjects plugin) implements TabExecutor {
                 case "skip" -> {
                     if (plugin.utils.prefs.getBoolean(Prefs.Option.WUP_ALLITEMS_RUNNING)) plugin.allItems.update(true);
                     else sender.sendMessage("§eThe AllItems project is not running.");
+                    return true;
+                }
+                case "reset" -> {
+                    Bukkit.broadcastMessage("§cResetting AllItems project.");
+                    Bukkit.broadcastMessage("§eStart a new AllItems project with \"/allitems new\".");
+                    plugin.utils.prefs.set(Prefs.Option.WUP_ALLITEMS_RUNNING, false, true);
+                    plugin.utils.prefs.remove(Prefs.Option.WUP_ALLITMES_ITEMS);
+                    plugin.utils.prefs.remove(Prefs.Option.WUP_ALLITEMS_OBTAINED);
+                    plugin.utils.prefs.remove(Prefs.Option.WUP_ALLITMES_INDEX);
+                    plugin.allItems = null;
                     return true;
                 }
             }
