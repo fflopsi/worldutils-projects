6d8e76732ff46fc8103cbf406a052f1f153ba41a
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-10-28 16:50:47 +0200

    add event handlers to EntityPickupItemEvent and PlayerJoinEvent

--- src/main/java/me/frauenfelderflorian/wuprojects/Listeners.java
+++ src/main/java/me/frauenfelderflorian/wuprojects/Listeners.java
@@ -1,15 +1,27 @@
 package me.frauenfelderflorian.wuprojects;
 
+import me.frauenfelderflorian.worldutils.config.Prefs;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
-public class Listeners implements Listener {
+public record Listeners(WUProjects plugin) implements Listener {
     @EventHandler
     public void onEntityPickupItemEvent(EntityPickupItemEvent event) {
-        if (event.getEntity() instanceof Player) {
-            //check if needed item, advance
+        if (event.getEntity() instanceof Player && plugin.utils.prefs.getBoolean(Prefs.Option.TIMER_RUNNING)
+                && plugin.utils.prefs.getBoolean(Prefs.Option.WUP_ALLITEMS_RUNNING)
+                && event.getItem().getItemStack().getType()
+                == plugin.utils.prefs.getList(Prefs.Option.WUP_ALLITMES_ITEMS)
+                .get(plugin.utils.prefs.getInt(Prefs.Option.WUP_ALLITMES_INDEX))) {
+            plugin.allItems.update(true);
         }
+    }
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        if (plugin.utils.prefs.getBoolean(Prefs.Option.WUP_ALLITEMS_RUNNING))
+            plugin.allItems.itemBar.addPlayer(event.getPlayer());
     }
 }
